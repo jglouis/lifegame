1d4def6e7a58d88b47db8006eb6d4e56f5c88468
Jean-Guillaume Louis <jguillame.louis@gmail.com>
2015-12-11 20:05:46 +0100

    Naive implementation of a life game.

---
+++ lifegame.go
@@ -0,0 +1,95 @@
+package main
+
+import "fmt"
+
+type Board [][]bool
+
+func New(size uint) Board {
+	b := make(Board, size)
+	for i := range b {
+		b[i] = make([]bool, size)
+	}
+	return b
+}
+
+func (b Board) String() string {
+	str := ""
+	for _, row := range b {
+		for _, cell := range row {
+			if cell {
+				str += "1"
+			} else {
+				str += "0"
+			}
+		}
+		str += "\n"
+	}
+	return str
+}
+
+// One tick og game of life
+func (b Board) Tick() {
+	// for each cell:
+	newBoard := New(3)
+	for i, row := range b {
+		for j, cell := range row {
+			if cell {
+				adj_alife := b.CountAdjacentCells(i, j, true)
+				if adj_alife == 2 || adj_alife == 3 {
+					newBoard[i][j] = true
+				} else {
+					{
+						newBoard[i][j] = false
+					}
+				}
+			} else {
+				adj_alife := b.CountAdjacentCells(i, j, true)
+				if adj_alife == 3 {
+					newBoard[i][j] = true
+				}
+			}
+		}
+	}
+	for i, row := range newBoard {
+		for j, cell := range row {
+			b[i][j] = cell
+		}
+	}
+
+}
+
+func (b Board) CountAdjacentCells(x, y int, isActive bool) int {
+	size := len(b)
+	count := 0
+	for i := x - 1; i <= x+1; i++ {
+		for j := y - 1; j <= y+1; j++ {
+			if x == i && y == j {
+				continue
+			}
+			if i < 0 || j < 0 || i > size-1 || j > size-1 {
+				continue
+			}
+			if b[i][j] == isActive {
+				count++
+			}
+
+		}
+	}
+	return count
+}
+
+func main() {
+	b := New(3)
+	b[1][0] = true
+	b[1][1] = true
+	b[1][2] = true
+
+	fmt.Println(b)
+	b.Tick()
+	fmt.Println(b)
+	b.Tick()
+	fmt.Println(b)
+	b.Tick()
+	fmt.Println(b)
+
+}
